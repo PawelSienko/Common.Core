832a1c095b4c5145ea62a4b5cdf19c995a970ed9
Pawe� Sienko <psienko@objectivity.co.uk>
2016-06-17 19:07:16 +0200

    Init core library.

---
+++ Common.Core/Consts.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Core
+{
+    public static class Consts
+    {
+        public static class MessageFormats
+        {
+            public const string ExceptionMessagePattern = "Parameter: {0}. Info:  {1]";
+        }
+    }
+}
---
+++ Common.Core/Exceptions/InvalidStatementException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Configuration;
+using System.Net.Http;
+
+namespace Common.Core.Exceptions
+{
+    public class InvalidStatementException : Exception
+    {
+        private const string DefaultMsg = "The statement doesn't match to conditions.";
+        private readonly string message;
+        private readonly string parameter;
+        
+        public InvalidStatementException()
+        {   
+        }
+
+        public InvalidStatementException(string message, string parameter)
+        {
+            this.parameter = parameter;
+            this.message = message;
+        }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(parameter) && string.IsNullOrEmpty(message))
+            {
+                return string.Format(Consts.MessageFormats.ExceptionMessagePattern, parameter, message);
+            }
+            return DefaultMsg;
+        }
+
+        public override string Message
+        {
+            get { return DefaultMsg; }
+        }
+    }
+}
